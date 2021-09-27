fa3b135c93d9640fe4bcc3642b4e047a153fbf72
Aleksandr Naumov <skisk8fmx@gmail.com>
2021-09-27 22:00:04 +0300

    models+migrations

--- products/models.py
+++ products/models.py
@@ -3,9 +3,11 @@ from django.db import models
 # Create your models here.
 
 
-class ProductCatergory(models.Model):
+class ProductCategory(models.Model):
     name = models.CharField(max_length=64, unique=True)
     description = models.TextField(blank=True)
+    def __str__(self):
+        return self.name
 
 class Product(models.Model):
     name = models.CharField(max_length=256)
@@ -13,4 +15,6 @@ class Product(models.Model):
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     quantity = models.PositiveIntegerField(default=0)
-    category = models.ForeignKey(ProductCatergory, on_delete=models.CASCADE)
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
