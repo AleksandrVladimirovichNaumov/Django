db395a3f46e7af56a21dad0234849f83139b18fe
Aleksandr Naumov <skisk8fmx@gmail.com>
2021-10-10 12:45:57 +0300

    admins: delete-update

--- admins/views.py
+++ admins/views.py
@@ -1,9 +1,14 @@
-
+from django.contrib.auth.decorators import user_passes_test
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
+from admins.forms import UserAdminRegisterForm, UserAdminProfileForm
+from geekshop.mixin import CustomDispatchMixin
 from users.models import User
 
 
@@ -21,11 +26,36 @@ class UserListView(ListView):
         context['title']= 'Панель Админимтратора | Пользователи'
         return context
 
-class UserCreateView(CreateView):
-    pass
+class UserCreateView(CreateView, CustomDispatchMixin):
+    model = User
+    template_name = 'admins/admin-users-create.html'
+    form_class = UserAdminRegisterForm
+    success_url = reverse_lazy('admins:admins_user')
 
-class UserUpdateView(UpdateView):
-    pass
+    def get_context_data(self,  *, object_list = None, **kwargs):
+        context = super(UserCreateView, self).get_context_data(**kwargs)
+        context['title']= 'Панель Админимтратора | Регистрация'
+        return context
 
-class UserDeleteView(DeleteView):
-    pass
+class UserUpdateView(UpdateView, CustomDispatchMixin):
+    model = User
+    template_name = 'admins/admin-users-update-delete.html'
+    form_class = UserAdminProfileForm
+    success_url = reverse_lazy('admins:admins_user')
+
+    def get_context_data(self,  *, object_list = None, **kwargs):
+        context = super(UserUpdateView, self).get_context_data(**kwargs)
+        context['title']= 'Панель Админимтратора | Обновление пользователя'
+        return context
+
+class UserDeleteView(DeleteView, CustomDispatchMixin):
+    model = User
+    template_name = 'admins/admin-users-read.html'
+    success_url = reverse_lazy('admins:admins_user')
+
+
+    def delete(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        self.object.is_active = False
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
