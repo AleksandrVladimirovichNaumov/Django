1af52c4c5f823cf7c68f0239fa6f4df7e49e80a0
Aleksandr Naumov <skisk8fmx@gmail.com>
2021-10-14 13:40:30 +0300

    context processor

--- users/models.py
+++ users/models.py
@@ -1,12 +1,25 @@
+from datetime import timedelta
+
 from django.db import models
 
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-
+from django.utils.timezone import now
 
 
 class User(AbstractUser):
     image = models.ImageField(upload_to='users_image', blank=True)
+
+    age = models.PositiveIntegerField(default=18)
+
+    activation_key = models.CharField(max_length=128, blank=True, null=True)
+    # activate_key_created = models.DateTimeField(default=(now()+timedelta(hours=48)))
+    activation_key_created = models.DateTimeField(auto_now_add=True, **NULL_INSTALL)
+
+    def us_activation_key_expired(self):
+        if now() <= self.activation_key_created + timedelta(hours=48):
+            return False
+        return True
 
 
