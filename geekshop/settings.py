3e9f1936a819d4e2c02d4134388e86377e3e296e
Aleksandr Naumov <skisk8fmx@gmail.com>
2021-10-30 18:03:19 +0300

    prepared for server

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -35,7 +35,7 @@ DEBUG = True
 # для проверки проверки возраста
 # ALLOWED_HOSTS = ['127.0.0.1']
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -92,10 +92,18 @@ WSGI_APPLICATION = 'geekshop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'geekshop',
+        'user': 'postgres'
     }
 }
 
