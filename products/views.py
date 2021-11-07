6232e51486e6928111f9b9533da86d67a1b564e1
Aleksandr Naumov <skisk8fmx@gmail.com>
2021-11-07 15:24:02 +0300

    memcached product

--- products/views.py
+++ products/views.py
@@ -10,7 +10,6 @@ from django.conf import settings
 from django.core.cache import cache
 
 
-
 # Create your views here.
 
 class ProductsListView(ListView):
@@ -32,27 +31,47 @@ class ProductsListView(ListView):
         else:
             return ProductCategory.objects.all()
 
+    @staticmethod
+    def get_links_product():
+        if settings.LOW_CACHE:
+            key = 'links_product'
+            links_product = cache.get(key)
+            if links_product is None:
+                links_product = Product.objects.all()
+                cache.set(key, links_product)
+            return links_product
+        else:
+            return Product.objects.all()
+
     def get_context_data(self, **kwargs):
         context = super(ProductsListView, self).get_context_data(**kwargs)
+        context['products'] = self.get_links_product()
         context['categories'] = self.get_links_category()
-        context['page_quantity'] = range(1, len(self.object_list)//self.paginate_by+2)
-        context['page_max'] = len(self.object_list)//self.paginate_by+1
+        context['page_quantity'] = range(1, len(self.object_list) // self.paginate_by + 2)
+        context['page_max'] = len(self.object_list) // self.paginate_by + 1
         # флаг для првоерки отфильтрованы ли продукты по категории
         context['is_categorised'] = True if self.kwargs.get('category_id') else False
         if self.kwargs.get('page_id'):
             context['page_obj'].number = self.kwargs.get('page_id')
-            context['page_obj'].object_list = self.object_list[context['page_obj'].number * self.paginate_by - self.paginate_by:self.paginate_by * context['page_obj'].number]
+            context['page_obj'].object_list = self.object_list[context[
+                                                                   'page_obj'].number * self.paginate_by - self.paginate_by:self.paginate_by *
+                                                                                                                            context[
+                                                                                                                                'page_obj'].number]
         # как оказалось нельзя фильтровать срезы после пагинации.
         # поэтому сначала фильтруем все объекты а потом делаем срез в зависимости от текущей страницы и шага пагинатора
         if self.kwargs.get('category_id'):
             context['current_category'] = self.kwargs.get('category_id')
             if self.kwargs.get('page_id'):
                 context['page_obj'].number = self.kwargs.get('page_id')
-            filtered_page_obj = self.object_list.filter(category_id=self.kwargs['category_id']).select_related('category')
+            filtered_page_obj = self.object_list.filter(category_id=self.kwargs['category_id']).select_related(
+                'category')
             context['page_quantity'] = range(1, len(filtered_page_obj) // self.paginate_by + 2)
             context['page_max'] = len(filtered_page_obj) // self.paginate_by + 1
             # делаем срез в зависимости от текущей страницы и шага пагинатора
-            context['page_obj'].object_list = filtered_page_obj[context['page_obj'].number*self.paginate_by - self.paginate_by:self.paginate_by*context['page_obj'].number]
+            context['page_obj'].object_list = filtered_page_obj[context[
+                                                                    'page_obj'].number * self.paginate_by - self.paginate_by:self.paginate_by *
+                                                                                                                             context[
+                                                                                                                                 'page_obj'].number]
         return context
 
     def get_price(self, pk):
@@ -63,6 +82,7 @@ class ProductsIndex(ListView):
     model = Product
     title = 'Каталог'
     template_name = 'products/index.html'
+
 #
 #
 #
@@ -94,4 +114,3 @@ class ProductsIndex(ListView):
 #
 #     context.update({'products':products_paginator})
 #     return django.shortcuts.render(request, 'products/products.html', context)
-
