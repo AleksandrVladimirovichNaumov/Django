547cc0223be3087e29c66b5998c78d7c038d2776
Aleksandr Naumov <skisk8fmx@gmail.com>
2021-11-07 22:57:48 +0300

    ORM F() 2

--- admins/views.py
+++ admins/views.py
@@ -90,12 +90,12 @@ class CategoryCreateView(CreateView, CustomDispatchMixin):
         return context
 
 
-class CategoryUpdateView(UpdateView):
+class CategoryUpdateView(UpdateView, CustomDispatchMixin):
     model = ProductCategory
     template_name = 'admins/admin-categories-update-delete.html'
     from_class = CategoryAdminUpdateForm
     context_object_name = 'category'
-    # fields = ('name', 'description', 'discount')
+    fields = '__all__'
     success_url = reverse_lazy('admins:admins_category')
 
     def get_context_data(self, **kwargs):
