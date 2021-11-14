645c317e24e288c4322b091b284ddcd6df11f515
Aleksandr Naumov <skisk8fmx@gmail.com>
2021-11-14 10:08:42 +0300

    feedbacks

--- feedbacks/views.py
+++ feedbacks/views.py
@@ -10,11 +10,10 @@ class FeedbackListView(ListView):
 
     model = Feedback
     template_name = 'feedbacks/feedbacks.html'
-    context_object_name = 'feedback'
 
     # def get_queryset(self):
     #     return Feedback.objects.filter(user=self.request.user).select_related('user')
 
-    # def get_context_data(self, *, object_list=None, **kwargs):
-    #     context = super(FeedbackListView, self).get_context_data()
-    #     context['feedbacks'] = Feedback.objects.filter(user=self.request.user).select_related('user')
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(FeedbackListView, self).get_context_data()
+        context['feedbacks'] = Feedback.objects.filter(user_id=self.request.user_id_id, product_id=self.request.product_id_id).select_related('user')
